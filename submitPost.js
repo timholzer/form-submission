9e3f20cc28a4050f13cb1db4eb11b2de5fc7f4db
Tim Holzer <timiholzer@gmail.com>
2021-06-08 18:13:29 -0600

    submission version

--- submitPost.js
+++ submitPost.js
@@ -1,36 +1,23 @@
 
 $(document).ready(function(){
 
-    $("#submit").on('click', function(){
+    $("#submit").on('click', function(event){
         event.preventDefault();
-   let formData = {
-        "emailConsent": true,
-        "birthDate": $('input[name=birthDate]').val(),
-        "email": $('input[name=email]').val(),
-        "name": $('input[name=name]').val(),
-        "id": 1
-        
-        
-    };
-//console.log(formData);
-    
-
-    //    $.ajax({
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json'
-    //     },
-    //     url: 'https://url.com/users/register', 
-    //     type : "POST", 
-    //     dataType : 'json', 
-    //     data : JSON.stringify(formData), 
-    //     success : function(result) {
-
-    //         console.log(result);
-    //     },
-    //     error: function(xhr, resp, text) {
-    //         console.log(xhr, resp, text);
-    //     }
-    //  })
+        addClearMessage();
+        //not sure if the 500 error I'm getting is an issue with my code or something on the server end.
+        $.post("https://my-json-server.typicode.com/JustUtahCoders/interview-users-api/users",
+        {
+            "emailConsent": true,
+            "birthDate": $('input[name=birthDate]').val(),
+            "email": $('input[name=email]').val(),
+            "name": $('input[name=name]').val(),
+            "id": 1
+        });     
 });
 });
+        
+function addClearMessage(){
+    document.getElementById("hidden").style.visibility = "visible";
+    document.getElementById("regform").reset();
+  
+}
